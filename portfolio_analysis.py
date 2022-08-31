78254b52f78f10d76df473d4a1e750d43f3a77b8
george_lee <43113880+leerock101@users.noreply.github.com>
2022-08-30 23:43:38 -0400

    adding fama french and current riskfree rate

--- portfolio_analysis.py
+++ portfolio_analysis.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
+import datetime
+import io
 
+import requests
 
 
 def get_ln_return(price_t,price_0,t=1):
@@ -45,23 +48,79 @@ def get_returns(some_iter,shift=1,n_periods=1):
     
     return return_series
     
-def get_current_risk_free_rate():
+def get_current_risk_free_rate(timeframe = '10 yr'):
+    """_summary_
 
-    return None
+    Args:
+        timeframe (str, optional): _description_. Defaults to ''.
+
+    Returns:
+        _type_: _description_
+    """
+    treasury_timeframes = [timeframe.lower() for timeframe in ['1 Mo', '2 Mo', '3 Mo', '6 Mo', '1 Yr', '2 Yr', '3 Yr', '5 Yr','7 Yr', '10 Yr', '20 Yr', '30 Yr']]
+
+
+    assert timeframe in treasury_timeframes, f"timeframe should be one one of these values {treasury_timeframes}"
+
+    current_year_month = datetime.datetime.today().strftime("%Y%m")
+    URL = f"https://home.treasury.gov/resource-center/data-chart-center/interest-rates/daily-treasury-rates.csv/all/{current_year_month}?type=daily_treasury_yield_curve&field_tdr_date_value_month={current_year_month}&page&_format=csv"
+    df = pd.read_csv(URL)
+
+    most_recent_row = df.iloc[0]
+
+    return most_recent_row[timeframe]/100
+
+def get_historical_risk_free():
+
+def get_fama_french(factors:int = 5,frequency="monthly")->pd.DataFrame:
+
+    if factors == 5:
+        if frequency == "monthly":
+            url = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_5_Factors_2x3_CSV.zip"
+        elif frequency == "daily":
+            url = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_5_Factors_2x3_daily_CSV.zip"
+        else:
+            raise ValueError
+
+    elif factors == 3:
+        if frequency == "monthly":
+            url = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_Factors_CSV.zip"
+        elif frequency == "weekly":
+            url = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_Factors_weekly_CSV.zip"
+        elif frequency == "daily":
+            url = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_Factors_daily_CSV.zip"
+        else:
+            raise ValueError
+    else:
+        raise ValueError
+
+    r = requests.get(url)
+    df = pd.read_csv(io.BytesIO(r.content), compression="zip", skiprows=[0,1,2] )
+
+    return df
 
 def sharpe_ratio(**kwargs):
-    '''
-    Sharpe Ratio/Safety First Ratio
+
+    """    
+    Sharpe Ratio
     
     Sharpe, William F. "Mutual Fund Performance." The Journal of Business 39, no. 1 (1966): 119-38. Accessed March 14, 2021. http://www.jstor.org/stable/2351741.
-    '''
-    
-    
-    risk_free_rate = kwargs.get("risk_free",get_current_risk_free_rate())
+    updated in 1994 "The Sharpe Ratio" https://web.stanford.edu/~wfsharpe/art/sr/SR.htm
 
-    returns = kwargs.get("period_return",kwargs.get("returns",[]))
-    variance = kwargs.get("period_variance",None)
-
-    ratio = (np.mean(returns) - risk_free_rate) / variance
+    Returns:
+        sharpe_ratio: float
+    """
     
-    return ratio
+    risk_free_rate = kwargs.get("risk_free_rate",get_current_risk_free_rate())
+    returns = kwargs.get("returns",[])
+
+    ddof = kwargs.get("ddof",1)
+
+    period_return = kwargs.get("period_return",np.mean(returns))
+    variance = kwargs.get("period_variance",np.var(returns,ddof=ddof))
+    stdev = kwargs.get("period_stdev",np.sqrt(variance))
+
+    print(period_return,risk_free_rate,stdev)
+    sharpe_ratio = ( period_return - risk_free_rate) / stdev
+    
+    return sharpe_ratio
